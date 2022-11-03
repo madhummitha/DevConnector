c2999e2caed37e989ca6e88082b9deb55f62d5f3
Madhummitha <madhummithar@gmail.com>
2022-11-03 23:52:20 +0530

    UI route

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+      </Routes>
+    </Fragment>
+  </Router>
+);
+
+export default App;
