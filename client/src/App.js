1eba89dc7905cbdb32d50b97d53864162a0e1ab7
Madhummitha <madhummithar@gmail.com>
2022-11-10 11:30:42 +0530

    Authentication base setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import "./App.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import "./App.css";
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-      </Routes>
+      <section className="container">
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </section>
     </Fragment>
   </Router>
 );
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
+
 // @route GET api/auth
 router.get("/", auth, async (req, res) => {
   try {
